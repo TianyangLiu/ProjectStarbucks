17dc71e27a7f9220660ec30edb6b5b4c511144ba
Tianyang Liu <lty0711@qq.com>
2016-12-08 17:58:44 -0800

    fixed a typo

--- application/models/Recipes.php
+++ application/models/Recipes.php
@@ -15,7 +15,7 @@ class Recipes extends CI_Model {
             array('id' => '6', 'menu' => 'Caramel Macchiato', 'item' => 'Caramel Sauce', 'Quantity' => '1'),
             array('id' => '7', 'menu' => 'Latte', 'item' => 'Milk', 'Quantity' => '1'),
             array('id' => '8', 'menu' => 'Latte', 'item' => 'Brewed Espresso', 'Quantity' => '2'),
-            array('id' => '9', 'menu' => 'Caffè Mocha', 'item' => 'Whipped Cream', 'Quantity' => '1'),
+            array('id' => '9', 'menu' => 'Mocha', 'item' => 'Whipped Cream', 'Quantity' => '1'),
             array('id' => '10', 'menu' => 'Mocha', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
             array('id' => '11', 'menu' => 'Mocha', 'item' => 'Mocha Sauce', 'Quantity' => '1'),
             array('id' => '12', 'menu' => 'Pumpkin Spice Latte', 'item' => 'Milk', 'Quantity' => '1'),
@@ -26,7 +26,37 @@ class Recipes extends CI_Model {
             array('id' => '17', 'menu' => 'White Chocolate Mocha', 'item' => 'Milk', 'Quantity' => '1'),
             array('id' => '18', 'menu' => 'White Chocolate Mocha', 'item' => 'White Chocolate Mocha Sauce', 'Quantity' => '1'),
             array('id' => '19', 'menu' => 'White Chocolate Mocha', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
-            array('id' => '20', 'menu' => 'White Chocolate Mocha', 'item' => 'Whipped Cream', 'Quantity' => '1')
+            array('id' => '20', 'menu' => 'White Chocolate Mocha', 'item' => 'Whipped Cream', 'Quantity' => '1'),
+            
+            // added more recipe by Tianyang Liu; 'item' is not proper
+            array('id' => '21', 'menu' => 'Blueberry Bar', 'item' => 'Pumpkin Spice Sauce', 'Quantity' => '1'),
+            array('id' => '22', 'menu' => 'Blueberry Bar', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
+            array('id' => '23', 'menu' => 'Blueberry Bar', 'item' => 'Whipped Cream', 'Quantity' => '1'),
+            array('id' => '24', 'menu' => 'Chocolate Cake Pop', 'item' => 'Milk', 'Quantity' => '1'),
+            array('id' => '25', 'menu' => 'Chocolate Cake Pop', 'item' => 'White Chocolate Mocha Sauce', 'Quantity' => '1'),
+            array('id' => '26', 'menu' => 'Chocolate Cake Pop', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
+            array('id' => '27', 'menu' => 'Chocolate Cake Pop', 'item' => 'Whipped Cream', 'Quantity' => '1'),
+            array('id' => '28', 'menu' => 'Blueberry Yogurt & Honey Muffin', 'item' => 'Pumpkin Spice Sauce', 'Quantity' => '1'),
+            array('id' => '29', 'menu' => 'Blueberry Yogurt & Honey Muffin', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
+            array('id' => '30', 'menu' => 'Blueberry Yogurt & Honey Muffin', 'item' => 'Whipped Cream', 'Quantity' => '1'),
+            array('id' => '31', 'menu' => 'Chocolate Mocha', 'item' => 'Milk', 'Quantity' => '1'),
+            array('id' => '32', 'menu' => 'Chocolate Mocha', 'item' => 'White Chocolate Mocha Sauce', 'Quantity' => '1'),
+            array('id' => '33', 'menu' => 'Chocolate Mocha', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
+            array('id' => '34', 'menu' => 'Chocolate Mocha', 'item' => 'Whipped Cream', 'Quantity' => '1'),
+            array('id' => '35', 'menu' => 'Lemon Loaf', 'item' => 'Pumpkin Spice Sauce', 'Quantity' => '1'),
+            array('id' => '36', 'menu' => 'Lemon Loaf', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
+            array('id' => '37', 'menu' => 'Lemon Loaf', 'item' => 'Whipped Cream', 'Quantity' => '1'),
+            array('id' => '38', 'menu' => 'Lemon Loaf', 'item' => 'Milk', 'Quantity' => '1'),
+            array('id' => '39', 'menu' => 'Chocolate Chunk Muffin', 'item' => 'White Chocolate Mocha Sauce', 'Quantity' => '1'),
+            array('id' => '40', 'menu' => 'Chocolate Chunk Muffin', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
+            array('id' => '41', 'menu' => 'Chocolate Chunk Muffin', 'item' => 'Whipped Cream', 'Quantity' => '1'),
+            array('id' => '42', 'menu' => 'Plain Bagel', 'item' => 'Pumpkin Spice Sauce', 'Quantity' => '1'),
+            array('id' => '43', 'menu' => 'Plain Bagel', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
+            array('id' => '44', 'menu' => 'Plain Bagel', 'item' => 'Whipped Cream', 'Quantity' => '1'),
+            array('id' => '45', 'menu' => 'Egg & Cheddar Breakfast Sandwich', 'item' => 'Milk', 'Quantity' => '1'),
+            array('id' => '46', 'menu' => 'Egg & Cheddar Breakfast Sandwich', 'item' => 'White Chocolate Mocha Sauce', 'Quantity' => '1'),
+            array('id' => '47', 'menu' => 'Egg & Cheddar Breakfast Sandwich', 'item' => 'Brewed Espresso', 'Quantity' => '1'),
+            array('id' => '48', 'menu' => 'Egg & Cheddar Breakfast Sandwich', 'item' => 'Whipped Cream', 'Quantity' => '1')
             
 	);
 
